6b03269ca180e01a430da0ddd4f3a55db0ac11c4
Jason Smith <jas@microsoft.com>
2018-08-07 15:11:22 -0700

    Get windows side rendering null

--- PreviewerWPF/MainWindow.xaml.cs
+++ PreviewerWPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,6 +28,36 @@ namespace PreviewerWPF
 			Forms.Init();
 
 			InitializeComponent();
+		}
+
+		private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+		{
+			// the the canvas and properties
+			var canvas = e.Surface.Canvas;
+
+			// get the screen density for scaling
+			var scale = (float)PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice.M11;
+			var scaledSize = new SKSize(e.Info.Width / scale, e.Info.Height / scale);
+
+			// handle the device screen density
+			canvas.Scale(scale);
+
+			//// make sure the canvas is blank
+			//canvas.Clear(SKColors.White);
+
+			//// draw some text
+			//var paint = new SKPaint
+			//{
+			//	Color = SKColors.Black,
+			//	IsAntialias = true,
+			//	Style = SKPaintStyle.Fill,
+			//	TextAlign = SKTextAlign.Center,
+			//	TextSize = 24
+			//};
+			//var coord = new SKPoint(scaledSize.Width / 2, (scaledSize.Height + paint.TextSize) / 2);
+			//canvas.DrawText("SkiaSharp", coord, paint);
+
+			Forms.Draw(null, Xamarin.Forms.Rectangle.Zero, e.Surface);
 		}
 	}
 }
