8b3130281793a833574f5704f79aea0d7366e5e3
James Clancey <james.clancey@gmail.com>
2018-08-09 00:25:34 -0700

    Added image cache

--- Xamarin.Forms.Platform.Skia/ImageCache.cs
+++ Xamarin.Forms.Platform.Skia/ImageCache.cs
@@ -5,10 +5,18 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
 namespace Xamarin.Forms.Platform.Skia
 {
 	public static class ImageCache
 	{
+		static ImageCache()
+		{
+			Directory.CreateDirectory(CacheFolder);
+		}
+		static string BaseCacheDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Previewer");
+		static string CacheFolder = Path.Combine(BaseCacheDirectory, "Cache");
 		static object locker = new object();
 		static Dictionary<string, SKBitmap> bitmaps = new Dictionary<string, SKBitmap>();
 		static Dictionary<string, TaskCompletionSource<bool>> tasks = new Dictionary<string, TaskCompletionSource<bool>>();
@@ -17,22 +25,38 @@ namespace Xamarin.Forms.Platform.Skia
 
 		public static SKBitmap TryGetValue(string url)
 		{
-			lock(locker)
+			lock (locker)
 			{
 				bitmaps.TryGetValue(url, out var bitmap);
+				if(bitmap == null)
+				{
+					bitmap = LoadFromLocalCache(url);
+					if (bitmap != null)
+						bitmaps[url] = bitmap;
+				}
 				return bitmap;
 			}
 		}
+
+		static SKBitmap LoadFromLocalCache(string url)
+		{
+			var caceFile = CacheFile(url);
+			if (File.Exists(caceFile))
+				return SKBitmap.Decode(caceFile);
+			return null;
+		}
+		static string CacheFile(string url) => Path.Combine(CacheFolder, MD5Hash(url));
 
 		public static Task<bool> LoadImage(string url, string requestId)
 		{
 			lock (locker)
 			{
-				if(!tasks.TryGetValue(url, out var task)){
+				if (!tasks.TryGetValue(url, out var task))
+				{
 					var tcs = new TaskCompletionSource<bool>();
-					task = tasks[url] =  tcs;
+					task = tasks[url] = tcs;
 					urlRequests[url] = requestId;
- 					urlQueue.Enqueue(url);
+					urlQueue.Enqueue(url);
 					RunDownloader();
 				}
 				return task.Task;
@@ -42,18 +66,18 @@ namespace Xamarin.Forms.Platform.Skia
 		static Task downloadTask;
 		static Task RunDownloader()
 		{
-			lock(locker)
+			lock (locker)
 			{
 				if (downloadTask?.IsCompleted ?? true)
-					downloadTask =  Task.Run(_runDownloader);
+					downloadTask = Task.Run(_runDownloader);
 				return downloadTask;
 			}
 		}
 		static async Task _runDownloader()
 		{
-			while(urlQueue.Count > 0)
+			while (urlQueue.Count > 0)
 			{
-				await downloadFirstUrl(); 
+				downloadFirstUrl();
 			}
 		}
 
@@ -62,14 +86,14 @@ namespace Xamarin.Forms.Platform.Skia
 		{
 			bool success = false;
 			var url = urlQueue.Dequeue();
+			var cache = CacheFile(url);
 			try
 			{
 				using (Stream stream = await httpClient.GetStreamAsync(url))
-				using (MemoryStream memStream = new MemoryStream())
+				using (var fileStream = new FileStream(cache, FileMode.Create, FileAccess.Write, FileShare.None))
 				{
-					await stream.CopyToAsync(memStream);
-					memStream.Seek(0, SeekOrigin.Begin);
-					var webBitmap = SKBitmap.Decode(memStream);
+					await stream.CopyToAsync(fileStream);
+					var webBitmap = SKBitmap.Decode(cache);
 					lock (locker)
 					{
 						bitmaps[url] = webBitmap;
@@ -80,6 +104,8 @@ namespace Xamarin.Forms.Platform.Skia
 			catch (Exception ex)
 			{
 				Console.WriteLine(ex);
+				if (File.Exists(cache))
+					File.Delete(cache);
 				success = false;
 			}
 			finally
@@ -96,8 +122,8 @@ namespace Xamarin.Forms.Platform.Skia
 		{
 			lock (locker)
 			{
-				var urls = urlRequests.Where(x => x.Value == requestId).Select(x=> x.Key).ToList();
-				foreach(var url in urls)
+				var urls = urlRequests.Where(x => x.Value == requestId).Select(x => x.Key).ToList();
+				foreach (var url in urls)
 				{
 					bitmaps.Remove(url);
 					var task = tasks[url];
@@ -108,6 +134,17 @@ namespace Xamarin.Forms.Platform.Skia
 				}
 			}
 
+		}
+		public static string MD5Hash(string input)
+		{
+			var hash = new StringBuilder();
+			var md5provider = new MD5CryptoServiceProvider();
+			var bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+			for (int i = 0; i < bytes.Length; i++)
+			{
+				hash.Append(bytes[i].ToString("x2"));
+			}
+			return hash.ToString();
 		}
 	}
 }
